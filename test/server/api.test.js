74a32d09e4e183e7e8d8a610b44d7418e4ae9181
Camille Teicheira <cteicheira@gmail.com>
2017-12-16 15:06:21 -0300

    add test for regions

--- test/server/api.test.js
+++ test/server/api.test.js
@@ -5,6 +5,13 @@ const test = require('tape')
 
 const app = require('../../src/server.js')
 
+/*
+  TO DO
+
+  * build and load test database with expected values
+  * 
+
+*/
 
 test('GET /api/countries', (t) => {
   request(app)
@@ -12,8 +19,30 @@ test('GET /api/countries', (t) => {
     .expect('Content-Type', /json/)
     .expect(200)
     .end(function(err, res) {
-      // test res.body
       t.error(err, 'No error')
+      var results = res.body;
+      t.equal(Object.keys(results).length, 214, '214 countries in the test database')
+      // why is `count` a string??
+      t.equal(results.Mexico.count, '172', 'Mexico has count')
+      t.equal(typeof results.Mexico.latitude, 'number', 'Mexico has latitude')
+      t.equal(typeof results.Mexico.longitude, 'number', 'Mexico has longitude')
+      t.end()
+    })
+})
+
+test('GET /api/regions', (t) => {
+  request(app)
+    .get('/api/regions')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end(function(err, res) {
+      t.error(err, 'No error')
+      var results = res.body;
+      t.equal(results.length, 9, '9 regions returned')
+      results.forEach((r) => {
+        t.ok(r.name, 'region ' + r.name+ ' has name')
+        t.ok(r.countries.length, 'region ' + r.name+ ' has countries')
+      })
       t.end()
     })
 })
