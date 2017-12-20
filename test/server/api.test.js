82c9eea55ebae942909cc1dc074e8e9321b409ab
Camille Teicheira <cteicheira@gmail.com>
2017-12-20 17:31:26 -0300

    explicit expectedfields, less code

--- test/server/api.test.js
+++ test/server/api.test.js
@@ -2,6 +2,7 @@
 
 const request = require('supertest')
 const test = require('tape')
+const _ = require('lodash')
 
 const app = require('../../src/server.js')
 
@@ -78,8 +79,8 @@ test('GET /api/regions', (t) => {
       const results = res.body
       t.equal(results.length, 9, '9 regions returned')
       results.forEach((r) => {
-        t.ok(r.name, 'region ' + r.name+ ' has name')
-        t.ok(r.countries.length, 'region ' + r.name+ ' has countries')
+        t.ok(r.name, 'region ' + r.name + ' has name')
+        t.ok(r.countries.length, 'region ' + r.name + ' has countries')
       })
       t.end()
     })
@@ -95,18 +96,21 @@ test('GET /api/placeTypes', (t) => {
       const results = res.body
       t.equal(results.length, 21, '21 placeTypes returned')
       results.forEach((r, i) => {
-        console.log(r.id)
-        t.ok(r.id, 'placeType \'' + r.name + '\' has id')
-        t.ok(r.identifier, 'placeType \'' + r.name + '\' has identifier')
-        t.ok(r.name, 'placeType \'' + r.name + '\' has name')
-        t.ok(r.name_regex, 'placeType \'' + r.name + '\' has name_regex')
-        t.ok(r.description, 'placeType \'' + r.name + '\' has description')
-        t.ok(r.places_count, 'placeType \'' + r.name + '\' has places_count')
-        t.ok(r.sort_order, 'placeType \'' + r.name + '\' has sort_order')
-        t.ok(r.created_at, 'placeType \'' + r.name + '\' has created_at')
-        t.ok(r.updated_at, 'placeType \'' + r.name + '\' has updated_at')
-        t.ok(r.icon, 'placeType \'' + r.name + '\' has icon')
-        t.ok(r.gpx_symbol, 'placeType \'' + r.name + '\' has gpx_symbol')
+        const expectedFields = [
+          'id',
+          'identifier',
+          'name',
+          'name_regex',
+          'description',
+          'places_count',
+          'sort_order',
+          'created_at',
+          'updated_at',
+          'icon',
+          'gpx_symbol'
+        ]
+        console.log('##', r.name)
+        testFields(t, expectedFields, r)
       })
       t.end()
     })
@@ -121,54 +125,57 @@ test('GET /api/blogs/:id', (t) => {
     .end(function(err, res) {
       t.error(err, 'No error')
       const results = res.body
-      t.equal(results.id, '1', '/api/blogs/1 returned blog with id 1')
-      t.equal(results.name, 'songoftheroad', 'blog 1 has name')
-      t.ok(results.domain, 'returns domain field')
-      t.ok(results.created_by_id, 'returns created_by_id field')
-      t.ok(results.created_at, 'returns created_at field')
-      t.ok(results.updated_at, 'returns updated_at field')
-      t.ok(results.check_ins, 'returns check_ins field')
-      t.ok(results.countries, 'returns countries field')
-      t.ok(results.placeCount, 'returns placeCount field')
-      t.ok(results.userName, 'returns userName field')
+      const expectedFields = [
+        'id',
+        'name',
+        'domain',
+        'description',
+        'created_by_id',
+        'countries',
+        'created_at',
+        'updated_at',
+        'placeCount',
+        'userName',
+        'vehicle',
+        'check_ins',
+        'profile_image_id',
+        'url'
+      ]
 
-      if (results.description.length === 0 || results.description) t.pass('returns optional description field')
-      else t.fail('returns description field')
-      if (results.vehicle === null || results.vehicle) t.pass('returns optional vehicle field')
-      else t.fail('returns vehicle field')
+      testFields(t, expectedFields, results)
       t.end()
     })
 })
 
-// !! THESE TESTS ARE COMMENTED OUT BECAUSE THEY HANG THE TESTS RIGHT NOW !!
+// // !! THESE TESTS ARE COMMENTED OUT BECAUSE THEY HANG THE TESTS RIGHT NOW !!
 
-// // GET api/download/{4 || mexico}/csv is totally broken
-// // Unhandled rejection SequelizeDatabaseError: relation "place_properties" does not exist
-// test('GET /api/download/:country/:format', (t) => {
-//   request(app)
-//     .get('/api/download/4/csv')
-//     .expect('Content-Type', /json/)
-//     .expect(200)
-//     .end(function(err, res) {
-//       t.error(err, 'No error')
-//       const results = res.body
-//       console.log(results)
-//       t.end()
-//     })
-// })
+// // // GET api/download/{4 || mexico}/csv is totally broken
+// // // Unhandled rejection SequelizeDatabaseError: relation "place_properties" does not exist
+// // test('GET /api/download/:country/:format', (t) => {
+// //   request(app)
+// //     .get('/api/download/4/csv')
+// //     .expect('Content-Type', /json/)
+// //     .expect(200)
+// //     .end(function(err, res) {
+// //       t.error(err, 'No error')
+// //       const results = res.body
+// //       console.log(results)
+// //       t.end()
+// //     })
+// // })
 
-// // GET api/download/{4 || mexico}/0 is totally broken
-// // Unhandled rejection SequelizeDatabaseError: relation "place_properties" does not exist
-// test('GET /api/search/:country/:page', (t) => {
-//   request(app)
-//     .get('/api/search/4/0')
-//     .expect('Content-Type', /json/)
-//     .expect(200)
-//     .end(function(err, res) {
-//       t.error(err, 'No error')
-//       t.end()
-//     })
-// })
+// // // GET api/download/{4 || mexico}/0 is totally broken
+// // // Unhandled rejection SequelizeDatabaseError: relation "place_properties" does not exist
+// // test('GET /api/search/:country/:page', (t) => {
+// //   request(app)
+// //     .get('/api/search/4/0')
+// //     .expect('Content-Type', /json/)
+// //     .expect(200)
+// //     .end(function(err, res) {
+// //       t.error(err, 'No error')
+// //       t.end()
+// //     })
+// // })
 
 
 test('GET /api/blogs/:id/check_ins/:page', (t) => {
@@ -179,23 +186,44 @@ test('GET /api/blogs/:id/check_ins/:page', (t) => {
     .end(function(err, res) {
       t.error(err, 'No error')
       const results = res.body
+
       t.equal(results.blogId, 1, '/api/blogs/1/check_ins/1 returned blog with id 1')
       t.equal(results.currentPage, 1, '/api/blogs/1/check_ins/1 returned page 1')
 
-      t.equal(results.blog.id, '1', '/api/blogs/1/check_ins/1 returned blog with id 1')
-      t.equal(results.blog.name, 'songoftheroad', 'blog 1 has name')
-      t.ok(results.blog.domain, 'returns domain field')
-      if (results.blog.description.length === 0 || results.blog.description) t.pass('returns optional description field')
-      else t.fail('returns description field')
-      t.ok(results.blog.created_at, 'returns created_at field')
-      t.ok(results.blog.updated_at, 'returns updated_at field')
-      t.ok(results.blog.created_by_id, 'returns created_by_id field')
+      let expectedFields = [
+        'blog',
+        'blogId',
+        'currentPage',
+        'pages',
+        'results',
+        'total'
+      ]
+      testFields(t, expectedFields, results)
 
-      t.ok(results.total, 'returns total field')
-      t.ok(results.pages, 'returns pages field')
-      t.ok(results.results && typeof results === 'object', 'returns results field')
-      t.ok(results.results[0], 'returns results field')
+      console.log('## results.blog')
+      expectedFields = [
+        'id',
+        'name',
+        'domain',
+        'description',
+        'created_at',
+        'created_by_id',
+        'updated_at',
+        'profile_image_id',
+        'url'
+      ]
+      testFields(t, expectedFields, results.blog)
 
+      console.log('## results.results[0]')
+      expectedFields = [
+        'check_in_translations',
+        'place',
+        'place_id',
+        'rating',
+        'visited'
+      ]
+
+      testFields(t, expectedFields, results.results[0])
       t.end()
     })
 })
@@ -219,3 +247,14 @@ test('GET /api/staticContent/:page', (t) => {
 test.onFinish((t)=>{
   app.closeDB()
 })
+
+function testFields(t, expected, results) {
+  const fields = Object.keys(results)
+  const inExpected = _.difference(expected.sort(), fields.sort())
+  const inResults = _.difference(fields.sort(), expected.sort())
+
+  if (inExpected.length) t.fail('fields "' + inExpected + '" are missing in query results')
+  else t.pass('all expected fields are present in query results')
+  if (inResults.length) t.fail('extra fields "' + inResults + '" are present in query results')
+  else t.pass('no extra fields are present in query results')
+}
